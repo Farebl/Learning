bc29353e2974b6b56912718d6cbfc2369e18cac0
Klymenko Dmytro <farebl.04@gmail.com>
2025-07-02 14:14:48 +0300

    Fix iteartor & Node and addi list::push_back/push_front
    For node:
    1) constructor was removed from struct.
    
    For base_iterator:
    1) was removed using type_;
    2) type order in std::conditional constructs was fixed;
    3) iterator pointer is now of type BaseNode* or const BaseNode*;
    4) pointer_type_ and reference_type_ were rewritten from BaseNode to template type T;
    5) constructor was moved to private section to prevent creation of free iterator;
    6) operator (*) now returns &Node.value, and operator (->) now returns Node*
    pointer; This is done via reinterpret_cast<Node*>(BaseNode*);
    
    For list:
    1) were added using (using) synonyms for working with STL;
    2) methods empty, push_back and push_front were added;

--- STL/Containers/list.cpp
+++ STL/Containers/list.cpp
@@ -13,7 +13,6 @@ class List{
     
     struct Node: BaseNode{
         T value;
-        Node (T value, BaseNode* prev = nullptr, BaseNode* next = nullptr): BaseNode(prev, next), value(value){}
     };
     BaseNode fake_node_;
     typename Alloc::template rebind<Node>::other alloc_;
@@ -24,25 +23,30 @@ public:
     template<bool IsConst = false>
     struct base_iterator{
     private:
-        using type_           = std::conditional<IsConst, const BaseNode, BaseNode>; 
-        using pointer_type_   = std::conditional<IsConst, BaseNode*, const BaseNode*>;
-        using refernece_type_ = std::conditional<IsConst, BaseNode&, const BaseNode&>;
+        using pointer_type_   = std::conditional<IsConst, const T*, T*>;
+        using refernece_type_ = std::conditional<IsConst, const T&, T&>;
         
-        pointer_type_ ptr_;
+        std::conditional<IsConst, const BaseNode*, BaseNode*> ptr_;
+        
+        
+        base_iterator(pointer_type_ ptr):ptr_(ptr){}
     public:
         using difference_type   = std::ptrdiff_t;
-        using value_type	    = type_;
+        using value_type	    = T;
         using pointer           = pointer_type_;
         using reference         = refernece_type_;
         using iterator_category = std::bidirectional_iterator_tag;
         
-        base_iterator(pointer_type_ ptr):ptr_(ptr){}
         
         base_iterator(const base_iterator& other) = default;
         base_iterator& operator=(const base_iterator& other) = default;
         
-        refernece_type_ operator*(){return *ptr_;};
-        pointer_type_ operator->(){return ptr_;};
+        refernece_type_ operator*(){
+            return reinterpret_cast<Node*>(ptr_)->value;
+        };
+        pointer_type_ operator->(){
+            return reinterpret_cast<Node*>(ptr_);
+        };
         
         base_iterator& operator++(){return 
             ptr_ = ptr_->next;
@@ -65,12 +69,82 @@ public:
         operator base_iterator<true>()const{return {ptr_};}
     };
 
+    using value_type	  = T;
+    using allocator_type  = Alloc;
+    using size_type       = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference	      = value_type&;
+    using const_reference = const value_type&;
+    using pointer         = std::allocator_traits<Alloc>::pointer;      	 
+    using const_pointer   = std::allocator_traits<Alloc>::const_pointer;
+
+    using iterator               = base_iterator<false>;	
+    using const_iterator         = base_iterator<true>;
+    using reverse_iterator       = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
     List() : fake_node_(&fake_node_, &fake_node_), alloc_(), sz_(0){}
-    
-    //List(size_t n) : fake_node_(&fake_node_, &fake_node_), alloc_(), sz_(n){}
-    
+   
+    bool empty(){return fake_node_.next == fake_node_.prev;}
+    void push_back(const T& value){
+        Node* new_node_ptr;
+        try{
+            new_node_ptr = std::allocator_traits<Alloc>::allocate(alloc_, 1);
+            try{
+                std::allocator_traits<Alloc>::construct(alloc_, &new_node_ptr->value, value);
+            }
+            catch(...){
+                std::allocator_traits<Alloc>::deallocate(alloc_, new_node_ptr, 1);
+                throw;
+            }
+        }
+        catch(...){
+            throw;
+        }
+        
+        if (empty()){
+            new_node_ptr->next = &fake_node_;
+            new_node_ptr->prev = &fake_node_;
+            fake_node_->next = new_node_ptr;
+            fake_node_->prev = new_node_ptr;
+        }
+        else{
+            Node* fake_node_prev = fake_node_->prev;  
+            new_node_ptr->prev = fake_node_prev;
+            new_node_ptr->next = &fake_node_;           
+            fake_node_->prev = new_node_ptr;
+        }
+        ++sz_;
+    }
 
+    void push_front(const T& value){
+        if (empty()){
+            push_back(value);
+        }
+        else{
+            Node* new_node_ptr;
+            try{
+                new_node_ptr = std::allocator_traits<Alloc>::allocate(alloc_, 1);
+                try{
+                    std::allocator_traits<Alloc>::construct(alloc_, &new_node_ptr->value, value);
+                }
+                catch(...){
+                    std::allocator_traits<Alloc>::deallocate(alloc_, new_node_ptr, 1);
+                    throw;
+                }
+            }
+            catch(...){
+                throw;
+            }
 
+            Node* fake_node_next = fake_node_->next;  
+            new_node_ptr->next = fake_node_next;           
+            new_node_ptr->prev = &fake_node_;
+            fake_node_->next = new_node_ptr;
+        }
+        ++sz_;
+    }
+    
 };
 
 int main(){
