049c9c493eaffbe91e201f7599b4eb2dca55e6f4
Klymenko Dmytro <farebl.04@gmail.com>
2025-07-03 15:23:48 +0300

    Improv iterators and list functions enhancements
    For structure (A):
    1) Added operator=(const A& other).
    
    iFor list:
    1) Since the rewrite (fllocator::rebind) in C++17, there was a problem
    with C++11 and C++14 support, which is now fixed by adding a check for
    the __cplucpluc macro. For C++17+, allocator_traits::rebind_alloc is used;
    2) Added a stub for operator=(const List& other), but it requires an
    implementation of the erase method, which is scheduled for the next commit;
    3) Added front/back methods;
    4) Changed the order of method declarations to match the order of their
    declarations in cppreference.
    
    For base_iterator:
    1) Added operator== to compare 2 iterators.

--- STL/Containers/list.cpp
+++ STL/Containers/list.cpp
@@ -18,10 +18,17 @@ class List{
         T value;
     };
     BaseNode fake_node_;
+
+#if (__cplusplus <=  201402L)
+    using NodeAlloc = typename Alloc::template rebind<Node>::other;
+#endif
+#if (__cplusplus >  201402L)
     using NodeAlloc = typename std::allocator_traits<Alloc>::template rebind_alloc<Node>;
+#endif
+    
     NodeAlloc alloc_;
     size_t sz_;
-    
+   
 public:
 
     template<bool IsConst = false>
@@ -30,8 +37,7 @@ public:
         friend class List;
         
         using pointer_type_   = typename std::conditional<IsConst, const T*, T*>::type;
-        using refernece_type_ = typename std::conditional<IsConst, const T&, T&>::type;
-        
+        using refernece_type_ = typename std::conditional<IsConst, const T&, T&>::type;        
         using BaseNode_t = typename std::conditional<IsConst, const BaseNode*, BaseNode*>::type;
         
         BaseNode_t ptr_;
@@ -50,29 +56,36 @@ public:
         
         refernece_type_ operator*(){
             return reinterpret_cast<Node*>(ptr_)->value;
-        };
+        }
         pointer_type_ operator->(){
             return reinterpret_cast<Node*>(ptr_);
-        };
+        }
         
         base_iterator& operator++(){ 
             ptr_ = ptr_->next;
             return *this;
-        };
+        }
         base_iterator& operator++(int ){ 
             base_iterator temp = *this;
             ptr_ = ptr_->next;
             return temp;
-        };
+        }
         base_iterator& operator--(){
             ptr_ = ptr_->prev;
             return *this;
-        };
+        }
         base_iterator& operator--(int ){ 
             base_iterator temp = *this;
             ptr_ = ptr_->prev;
             return temp;
-        };
+        }
+      
+
+        template<bool OtherIsConst>
+        bool operator==(const base_iterator<OtherIsConst>& other){
+            return ptr_ == other.ptr_;
+        }  
+        
         operator base_iterator<true>()const{return {ptr_};}
     };
 
@@ -93,8 +106,101 @@ public:
     List() : fake_node_(&fake_node_, &fake_node_), alloc_(), sz_(0){}
     ~List(){clear();}
 
-    bool empty(){return sz_ == 0;}
+    List& operator=(const List& other){
+        if (this == &other) return other;
+
+        Alloc new_alloc = std::allocator_traits<Alloc>::propagate_on_container_copy_assignment::value
+            ? other.alloc_ : alloc_;
+        
+        if(alloc_ == other.alloc_){
+            try{
+                if (sz_ >= other.sz_){
+                    auto this_it = begin();
+                    auto other_it = other.cbegin();
+                    auto other_end = other.end();
+                    for (; other_it != other_end; ++other_it, ++this_it){
+                        
+                    }
+                }
+            }
+            catch(...){throw;}
+        }
+
+        
+
+#ifdef LOGGING
+        std::cout<<"operator=(const List& other)" 
+            <<"\n\tthis - " << this
+            <<"\n\tother - "<< &other 
+            <<"\n"; 
+#endif    
+    }
+
+
+
+    reference front(){
+        return *begin();
+    }
+    const_reference front() const {
+        return *cbegin();
+    }
+
+    reference back(){
+        return *(--end());
+    }
+    const_reference cback() const {
+        return *(--cend());
+    }
+ 
     
+
+    iterator begin(){return {fake_node_.next};}
+    iterator end(){return &fake_node_;}
+    
+    const_iterator begin() const {return fake_node_.next;}
+    const_iterator end() const {return &fake_node_;}
+
+    const_iterator cbegin() const {return fake_node_.next;}
+    const_iterator cend() const {return &fake_node_;}
+
+
+    /*
+    The std::make_reverse_iterator function in C++, when creating a reverse iterator,
+    automatically shifts the iterator so that it points to an element that matches the 
+    logic of the reverse iterator.
+    */
+    reverse_iterator rbegin(){return std::make_reverse_iterator(begin());}
+    reverse_iterator rend(){return std::make_reverse_iterator(end());}
+    
+    const_reverse_iterator rbegin() const {return std::make_reverse_iterator(cbegin());}
+    const_reverse_iterator rend() const {return std::make_reverse_iterator(cend());}
+    
+    const_reverse_iterator crbegin() const {return std::make_reverse_iterator(cbegin());}
+    const_reverse_iterator crend() const {return std::make_reverse_iterator(cend());}
+
+
+
+
+    bool empty(){return sz_ == 0;}
+   
+    size_t size(){return sz_;}
+    
+    long max_size() const{return std::numeric_limits<difference_type>::max();}
+
+ 
+
+
+    void clear(){
+        while(!empty()){
+            pop_front();
+            pop_back();
+        }
+    }
+
+    //insert
+    //emplace
+    //erase
+
     
     void push_back(const T& value){
         Node* new_node_ptr;
@@ -131,6 +237,28 @@ public:
 #endif    
     }
 
+    //emplace_back()
+
+    void pop_back(){
+        if (empty()) {return;}
+
+        Node* delete_node = static_cast<Node*>(fake_node_.prev);
+#ifdef LOGGING
+        std::cout<<"Popping from back: " << delete_node <<"\n";
+#endif
+         fake_node_.prev = delete_node->prev;
+        fake_node_.prev->next = &fake_node_; 
+        std::allocator_traits<NodeAlloc>::destroy(alloc_, delete_node);
+        std::allocator_traits<NodeAlloc>::deallocate(alloc_, delete_node, 1);
+        --sz_;
+#ifdef LOGGING
+        std::cout<<"Popped from back: " << delete_node <<"\n";
+#endif
+     }
+    
+
+
+
     void push_front(const T& value){
         if (empty()){
             push_back(value);
@@ -164,23 +292,9 @@ public:
     }
 
 
-    void pop_back(){
-        if (empty()) {return;}
+    //emplace_front()
 
-        Node* delete_node = static_cast<Node*>(fake_node_.prev);
-#ifdef LOGGING
-        std::cout<<"Popping from back: " << delete_node <<"\n";
-#endif
-         fake_node_.prev = delete_node->prev;
-        fake_node_.prev->next = &fake_node_; 
-        std::allocator_traits<NodeAlloc>::destroy(alloc_, delete_node);
-        std::allocator_traits<NodeAlloc>::deallocate(alloc_, delete_node, 1);
-        --sz_;
-#ifdef LOGGING
-        std::cout<<"Popped from back: " << delete_node <<"\n";
-#endif
-     }
-    
+
     void pop_front(){
         if (empty()) {return;}
 
@@ -198,46 +312,31 @@ public:
 #endif
     }
 
-    void clear(){
-        while(!empty()){
-            pop_front();
-            pop_back();
-        }
-    }
+   
+    //resize
+   
+   
+    //swap
+   
+   
+    //merge
     
-    size_t size(){return sz_;}
-    long max_size() const{
-        return std::numeric_limits<difference_type>::max();
-    }
-
-
-
- 
-    iterator begin(){return {fake_node_.next};}
-    iterator end(){return &fake_node_;}
     
-    const_iterator begin() const {return fake_node_.next;}
-    const_iterator end() const {return &fake_node_;}
-
-    const_iterator cbegin() const {return fake_node_.next;}
-    const_iterator cend() const {return &fake_node_;}
-
-
-    /*
-    The std::make_reverse_iterator function in C++, when creating a reverse iterator,
-    automatically shifts the iterator so that it points to an element that matches the 
-    logic of the reverse iterator.
-    */
-    reverse_iterator rbegin(){return std::make_reverse_iterator(begin());}
-    reverse_iterator rend(){return std::make_reverse_iterator(end());}
+    //splice
     
-    const_reverse_iterator rbegin() const {return std::make_reverse_iterator(cbegin());}
-    const_reverse_iterator rend() const {return std::make_reverse_iterator(cend());}
     
-    const_reverse_iterator crbegin() const {return std::make_reverse_iterator(cbegin());}
-    const_reverse_iterator crend() const {return std::make_reverse_iterator(cend());}
+    //remove
+    
 
+    //remove_if
+
+
+    //unique
+
+    //sort
+   
 };
+
 
 struct A{
     int a;
@@ -253,10 +352,19 @@ struct A{
         std::cout<<"A(" << a << "): "<< this << "\n";
 #endif
     }
+
     A(const A& other): a(other.a){
 #ifdef LOGGING
         std::cout<<"A(copy of A(" << other.a << ")): "<< this << "\n";
 #endif
+    }
+    
+    A& operator=(const A& other) {
+        a = other.a;
+#ifdef LOGGING
+        std::cout<<"operator=(copy of A(" << other.a << ")): "<< this << "\n";
+#endif
+        return *this;
     }
 
     ~A(){
@@ -267,6 +375,6 @@ struct A{
 };
 
 int main(){
-
+    
     return 0;
 }
