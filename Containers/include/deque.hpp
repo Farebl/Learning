3057e2c60ee17043b9dac562438f85d970f9c8a0
Klymenko Dmytro <farebl.04@gmail.com>
2025-12-31 12:42:28 +0000

    fix(deque): base_iterator +=/-= operators
    Was fixed a problem, when a negative value was passed to the operator
    in operator+= or -=  -->  it led to an infinite loop and, as a
    consequence --> a stack overflow.

--- Containers/include/deque.hpp
+++ Containers/include/deque.hpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <memory>
 #include <limits>
+#include <cmath>
 
 
 namespace Farebl{
@@ -159,7 +160,8 @@ private:
         
         
         base_iterator& operator+=(difference_type value) & {
-            if (value < 0) return *this -= value;
+            if (value < 0) {return *this -= std::abs(value);}
+
             if (m_buckets_ptr != nullptr){
                 if (m_ptr != nullptr){
                     difference_type result_index = (const_cast<T*>(m_ptr) - *m_bucket_ptr) + (value % BucketSize);
@@ -257,7 +259,8 @@ private:
         }
 
         base_iterator& operator-=(difference_type value) & {
-            if (value < 0) {return *this += value;}
+            if (value < 0) {return *this += std::abs(value);}
+
             if (m_buckets_ptr != nullptr){
                 if (m_ptr != nullptr){
                     difference_type result_index_in_bucket = (const_cast<T*>(m_ptr) - *m_bucket_ptr) - (value % BucketSize);
